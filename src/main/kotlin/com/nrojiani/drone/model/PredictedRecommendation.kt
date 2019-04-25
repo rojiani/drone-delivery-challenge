6215d4914a54e267a3c01b86c0777ddff72de771
Navid Rojiani <nrojiani@gmail.com>
2019-04-25 11:34:39 -0400

    PredictedRecommendation

--- src/main/kotlin/com/nrojiani/drone/model/PredictedRecommendation.kt
+++ src/main/kotlin/com/nrojiani/drone/model/PredictedRecommendation.kt
@@ -1,20 +1,21 @@
 package com.nrojiani.drone.model
 
 /**
- * Likelihood of recommending service
+ * Likelihood of recommending service.
  * @param deliveryTimeRange delivery time range (in minutes)
  */
 enum class PredictedRecommendation(val deliveryTimeRange: IntRange) {
     PROMOTER(0 until 90),
     NEUTRAL(90 until 225),
-    DETRACTOR(225 until Int.MAX_VALUE);
+    DETRACTOR(225..Int.MAX_VALUE);
 
+    /** Factory methods */
     companion object {
         fun fromDeliveryTime(minutes: Int): PredictedRecommendation {
             require(minutes >= 0) { "Invalid delivery time: $minutes (must be >= 0)"}
-            return PredictedRecommendation.values().first {
-                minutes in it.deliveryTimeRange
-            }
+            return values().first { minutes in it.deliveryTimeRange }
         }
+
+        fun fromDeliveryTime(hours: Double): PredictedRecommendation = fromDeliveryTime(minutes = (hours * 60).toInt())
     }
 }
--- src/test/kotlin/com/nrojiani/drone/model/PredictedRecommendationTest.kt
+++ src/test/kotlin/com/nrojiani/drone/model/PredictedRecommendationTest.kt
@@ -1,12 +1,41 @@
 package com.nrojiani.drone.model
 
+import com.nrojiani.drone.model.PredictedRecommendation.*
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.lang.IllegalArgumentException
+import kotlin.test.assertFailsWith
 
 class PredictedRecommendationTest {
 
     @Test
-    fun getDeliveryTimeRange() {
+    fun fromDeliveryTime() {
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(0))
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(20))
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(89))
+        assertEquals(NEUTRAL, PredictedRecommendation.fromDeliveryTime(90))
+        assertEquals(NEUTRAL, PredictedRecommendation.fromDeliveryTime(200))
+        assertEquals(DETRACTOR, PredictedRecommendation.fromDeliveryTime(225))
+        assertEquals(DETRACTOR, PredictedRecommendation.fromDeliveryTime(Int.MAX_VALUE))
+
+        assertFailsWith<IllegalArgumentException> {
+            PredictedRecommendation.fromDeliveryTime(-1)
+        }
+    }
+
+    @Test
+    fun fromDeliveryTimeInHours() {
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(0.0))
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(1.0))
+        assertEquals(PROMOTER, PredictedRecommendation.fromDeliveryTime(1.49))
+        assertEquals(NEUTRAL, PredictedRecommendation.fromDeliveryTime(1.5))
+        assertEquals(NEUTRAL, PredictedRecommendation.fromDeliveryTime(3.7))
+        assertEquals(DETRACTOR, PredictedRecommendation.fromDeliveryTime(3.75))
+        assertEquals(DETRACTOR, PredictedRecommendation.fromDeliveryTime(10.0))
+
+        assertFailsWith<IllegalArgumentException> {
+            PredictedRecommendation.fromDeliveryTime(-1.0)
+        }
     }
 }
