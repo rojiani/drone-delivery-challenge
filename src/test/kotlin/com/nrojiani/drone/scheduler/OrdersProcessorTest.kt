28c562e45d2429be2b8e998ce360fc9a16e1b40f
Navid Rojiani <nrojiani@gmail.com>
2019-04-26 23:09:36 -0400

    OrderProcessorTest

---
+++ src/test/kotlin/com/nrojiani/drone/scheduler/OrdersProcessorTest.kt
@@ -0,0 +1,32 @@
+package com.nrojiani.drone.scheduler
+
+import com.nrojiani.drone.model.DRONE_LAUNCH_FACILITY_LOCATION
+import com.nrojiani.drone.model.DRONE_SPEED_BLOCKS_PER_SECOND
+import com.nrojiani.drone.scheduler.calculator.TransitTimeCalculator
+import com.nrojiani.drone.testutils.OrderData
+import org.junit.Test
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Ignore
+import kotlin.test.fail
+
+class OrdersProcessorTest {
+
+    private lateinit var ordersProcessor: OrdersProcessor
+
+    @Before
+    fun setUp() {
+        ordersProcessor = OrdersProcessor(
+            OrderData.ORDERS,
+            TransitTimeCalculator(DRONE_SPEED_BLOCKS_PER_SECOND),
+            DRONE_LAUNCH_FACILITY_LOCATION
+        )
+    }
+
+    @Test
+    fun calculateTransitTimes() {
+        val pendingDeliveries = ordersProcessor.calculateTransitTimes()
+        assertEquals(OrderData.ORDERS_WITH_TRANSIT_TIMES, pendingDeliveries)
+    }
+}
